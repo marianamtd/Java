85f76789a85a3bd671abe28936623b0a7da00017
marianamtd <mari.duarte2025@gmail.com>
2021-06-15 11:17:30 -0300

    Update PrimeiroPrograma.java

--- curso/PrimeiroPrograma.java
+++ curso/PrimeiroPrograma.java
@@ -1,9 +1,5 @@
-import org.graalvm.compiler.lir.aarch64.AArch64Move.StoreOp;
-
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
-
 public class PrimeiroPrograma{
-    public static void main (String[] args){
-        System.out.print("Meu jesus");
-    }
+   public static void main(String[] args){
+       System.out.print("Olá, Mundão!");
+   }
 }
