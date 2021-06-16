7da5f3ccb65802a1f0e8ab8bdecb5abd1577d54c
marianamtd <mari.duarte2025@gmail.com>
2021-06-16 10:17:31 -0300

    Create primaryStage.java

---
+++ curso/JavaFX/src/primaryStage.java
@@ -0,0 +1,17 @@
+package JavaFX.src;
+
+import javafx.scene.Scene;
+
+public enum primaryStage {
+    ;
+
+    public static void setScene(Scene scene) {
+    }
+
+    public static void setTittle(String string) {
+    }
+
+    public static void show() {
+    }
+
+}
