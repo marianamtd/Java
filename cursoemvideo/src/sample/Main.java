06b4910e22fe5c24e6c90e6df551f67f893129ac
marianamtd <mari.duarte2025@gmail.com>
2021-06-18 09:54:39 -0300

    Update Main.java

--- cursoemvideo/src/sample/Main.java
+++ cursoemvideo/src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +17,19 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Academia dos Dev");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        Button btn = new Button("Clique aqui");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Clicou no botão");
+            }
+        });
+
+        StackPane roots = new StackPane();
+        roots.getChildren().addAll(btn);
+        primaryStage.setScene(new Scene(roots, 300,250));
         primaryStage.show();
     }
 
