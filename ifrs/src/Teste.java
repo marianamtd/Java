0eaadaa94e7eda7a646afb9783a08ce79cf0d6ad
marianamtd <mari.duarte2025@gmail.com>
2021-06-18 16:03:53 -0300

    Update Teste.java

--- ifrs/src/Teste.java
+++ ifrs/src/Teste.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Teste {
+    public static void main(String[] args){
+        Scanner entrada;
+        entrada = new Scanner(System.in);
+        int n1, n2;
+        n1 = entrada.nextInt();
+        n2 = entrada.nextInt();
+        int soma = n1 + n2;
+        System.out.print(soma);
+    }
 }
