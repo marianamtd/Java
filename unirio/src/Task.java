6b0fb2a3bceba98ea38d3197770a4ea96764fe2b
marianamtd <mari.duarte2025@gmail.com>
2021-06-20 11:17:58 -0300

    .

---
+++ unirio/src/Task.java
@@ -0,0 +1,7 @@
+public class Task {
+    public static void main(String[] args) {
+        //Comentários
+        //Mais comentários para aprender a usar
+        System.out.print("Hello, Java");
+    }
+}
