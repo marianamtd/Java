82b01f7770caf1f50c248015685d8d013cdfb9dd
marianamtd <mari.duarte2025@gmail.com>
2021-06-21 09:01:40 -0300

    .

---
+++ unirio/src/Robots.java
@@ -0,0 +1,13 @@
+/**public class Robots{
+ public static void main (String[] args){
+ System.out.println("WE are robots");
+ }
+ } */
+
+public class Robots{
+    public static void main(String[] args){
+        System.out.println("Cats are robots");
+    }
+}
+
+
