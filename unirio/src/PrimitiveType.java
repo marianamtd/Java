08f7f3f6eb023ce86e3d10058fb8f86a592d61c7
marianamtd <mari.duarte2025@gmail.com>
2021-06-23 15:36:51 -0300

    Create PrimitiveType.java

---
+++ unirio/src/PrimitiveType.java
@@ -0,0 +1,13 @@
+public class PrimitiveType {
+    public static void main(String[] args){
+        int decimalNumber = 500;
+        int hexNumber = 0xFF;
+        int binaryNumber = 00111;
+        int octalNumber = 0335;
+        long longNumber = 33L; //l or L is a literal forr Longs
+        long bigNumber = 100_000_000_000L; // _ is the underscore
+
+        int intZero;
+        long longZero;
+    }
+}
