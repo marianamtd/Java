86870d57a1073610a96030eb74cebf87b99ba641
marianamtd <mari.duarte2025@gmail.com>
2021-06-24 20:21:59 -0300

    .

---
+++ unirio/src/Desk.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Desk {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println((scanner.nextInt() + 1)/2 + (scanner.nextInt()+ 1)/2 + (scanner.nextInt() +1)/2);
+    }
+}
+
--- unirio/src/Even.java
+++ unirio/src/Even.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Even {
     public static void main(String[] args){
-        
+        //input
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        //quando for impar
+        if (num % 2 == 1){
+            num++;
+        }
+        else{
+            num += 2;
+        }
+
+        System.out.println(num);
     }
 }
