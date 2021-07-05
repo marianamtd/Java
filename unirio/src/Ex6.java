fd61385565603dc52fc7520f74817cd622edd1ca
marianamtd <mari.duarte2025@gmail.com>
2021-07-05 09:27:18 -0300

    Create Ex6.java

---
+++ unirio/src/Ex6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Distância: ");
+        float distancia = scanner.nextFloat();
+        System.out.println("Tempo gasto [horas]: ");
+        float tempo = scanner.nextFloat();
+        System.out.println("Gasolina consumida [litros]: ");
+        float gasolina_gasta = scanner.nextFloat();
+
+        System.out.println("Velocidade:  " + distancia/ tempo);
+        System.out.print("Gasolina: " +);
+    }
+}
