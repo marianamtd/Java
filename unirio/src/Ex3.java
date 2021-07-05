1ae763d20d71e5ce84f90aef1a3b193d6f9a927e
marianamtd <mari.duarte2025@gmail.com>
2021-07-05 09:27:31 -0300

    Update Ex3.java

--- unirio/src/Ex3.java
+++ unirio/src/Ex3.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Ex3 {
     public static void main(String[] args){
-        Scanner scanner =
+        Scanner oioi = new Scanner(System.in);
+        float salario = oioi.nextFloat();
+        float percentual = oioi.nextFloat();
+
+        System.out.println(((salario * percentual)/100) + salario);
+
     }
 }
