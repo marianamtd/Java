2cd9802c4dc1aadde5896796e309a6360b89dcb0
marianamtd <mari.duarte2025@gmail.com>
2021-07-05 11:27:21 -0300

    Create Bus.java

---
+++ unirio/src/Bus.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Bus {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Velocidade do bus: " + (scanner.nextFloat()/scanner.nextFloat()));
+    }
+}
