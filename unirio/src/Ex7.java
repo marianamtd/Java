42d53144fc9695142934705da2111d2007db3c85
marianamtd <mari.duarte2025@gmail.com>
2021-07-12 10:05:15 -0300

    Create Ex7.java

---
+++ unirio/src/Ex7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() % 2 == 0? "Par" : "Impar");
+
+    }
+}
