23c62beb647d7e35d7938c73bc06594199c8efa7
marianamtd <mari.duarte2025@gmail.com>
2021-07-15 08:19:46 -0300

    Update Palindrome.java

--- unirio/src/Palindrome.java
+++ unirio/src/Palindrome.java
@@ -1,7 +1,19 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+
+        //comparando a 1 e a 4 pos
+       if ((numero / 1000) == (numero % 10) && (numero/ 100 % 10) == (numero/ 10 % 10) ||(numero / 1000 == 0) && (numero % 10 == 0)){
+            System.out.println("1");
+        }
+        else{
+            System.out.println(new Random().nextInt(100));
+        }
+
     }
 }
