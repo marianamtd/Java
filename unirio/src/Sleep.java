827d9a5f0421186a8b29d7c7e67b2ec8f02504f1
marianamtd <mari.duarte2025@gmail.com>
2021-07-15 08:19:43 -0300

    Create Sleep.java

---
+++ unirio/src/Sleep.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Sleep {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int hourA = scanner.nextInt();  // normal
+        int hourB = scanner.nextInt(); // excess
+        int sleep = scanner.nextInt(); // Ann hours of sleep
+
+        if (hourA <= hourB){
+            if (sleep >= hourA && sleep <= hourB) {
+                System.out.printf("Normal");
+            }
+            else if (sleep < hourA){
+                System.out.printf("Deficiency");
+            }
+            else if (sleep > hourA && sleep > hourB){
+                System.out.printf("Excess");
+            }
+        }
+    }
+}
