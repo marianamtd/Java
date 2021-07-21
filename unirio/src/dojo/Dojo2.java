ae4237e7348cf7eb4c9c2db952d790f30b87d249
marianamtd <mari.duarte2025@gmail.com>
2021-07-21 17:56:30 -0300

    Update Dojo2.java

--- unirio/src/dojo/Dojo2.java
+++ unirio/src/dojo/Dojo2.java
@@ -2,23 +2,34 @@ package dojo;
 import java.util.Scanner;
 
 public class Dojo2 {
-
-    static Comparable<String> alunoAprovado(double media) {
-        return media < 4 ? "O aluno reprovou"
-                : media < 7 ? "O aluno ficou de prova final"
-                :"O aluno passou direto";
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Insira as notas: ");
         double n1 = scanner.nextDouble();
         double n2 = scanner.nextDouble();
         double n3 = scanner.nextDouble();
         double media = (n1 + n2 + n3) / 3;
 
-        System.out.print(alunoAprovado(alunoAprovado(media)));
+        if (media >= 7) {
+            System.out.print("O aluno passou direto.");
+
+        }else if (media > 4) {
+            System.out.print("O aluno ficou de prova final.\n");
+            System.out.print("Insira as notas: ");
+            double prova_f = scanner.nextDouble();
+
+            if ((prova_f + media) / 2 >= 5) {
+                System.out.print("O aluno passou.");
+            }
+            else {
+                System.out.print("O aluno reprovou.");
+            }
 
 
+
+        }else {
+            System.out.print("O aluno reprovou.");
+        }
     }
 }
