c13d36985086ea62a65a106569ce8de2fc1dfb92
marianamtd <mari.duarte2025@gmail.com>
2021-07-21 18:09:32 -0300

    Dojo2

--- unirio/src/dojo/Dojo2.java
+++ unirio/src/dojo/Dojo2.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class Dojo2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //critérios de avaliação do usuário
+        System.out.print("Insira as notas de avaliação(mínima para passar direto, mínima para fazer prova final, e mínima para passar na média final)\n");
+        double direto = scanner.nextDouble();
+        double min_prova_f = scanner.nextDouble();
+        double min_media_f = scanner.nextDouble();
 
         System.out.print("Insira as notas: ");
         double n1 = scanner.nextDouble();
@@ -11,23 +16,21 @@ public class Dojo2 {
         double n3 = scanner.nextDouble();
         double media = (n1 + n2 + n3) / 3;
 
-        if (media >= 7) {
+        //passou pela media já estipulada ou pelo novo critério
+        if (media >= 7 || media >= direto) {
             System.out.print("O aluno passou direto.");
 
-        }else if (media > 4) {
+        }else if (media > 4 || media >= min_prova_f) {
             System.out.print("O aluno ficou de prova final.\n");
             System.out.print("Insira as notas: ");
             double prova_f = scanner.nextDouble();
 
-            if ((prova_f + media) / 2 >= 5) {
+            if ((prova_f + media) / 2 >= 5 || media >= min_media_f) {  //verifica se passou na prova final
                 System.out.print("O aluno passou.");
             }
             else {
                 System.out.print("O aluno reprovou.");
             }
-
-
-
         }else {
             System.out.print("O aluno reprovou.");
         }
