41e93c05e850884f1161b5792395695be45ed765
marianamtd <mari.duarte2025@gmail.com>
2021-07-22 09:30:07 -0300

    Dojo3

--- unirio/src/dojo/Dojo3.java
+++ unirio/src/dojo/Dojo3.java
@@ -1,4 +1,5 @@
 package dojo;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -32,6 +33,63 @@ import java.util.Scanner;
 } */
 
 //utilizando numeros aleatórios
+/*public class Dojo3 {
+    public static void main (String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int media = 0, soma = 0;
+
+
+        for (int i = 0; i < 5 ; i++) {
+            int aleatorio = random.nextInt(10);
+            soma += aleatorio;
+            media = soma / 5;
+        }
+        System.out.print("Digite um número: ");
+        int n = scanner.nextInt();
+        while (n != media) {
+
+            if (n < media) {
+                System.out.print("Menor\n");
+
+            }else {
+                System.out.print("Maior\n");
+            }
+            System.out.print("Digite um número: ");
+            n = scanner.nextInt();
+        }
+        System.out.print("Acertou!");
+
+    }
+} */
+
+//utilizando num alteatórios (qtd <= 15)
 public class Dojo3 {
-    public static void main (String[])
+    public static void main (String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int media = 0, soma = 0;
+
+
+        for (int i = 0; i < 5 ; i++) {
+            int aleatorio = random.nextInt(10);
+            soma += aleatorio;
+            media = soma / 5;
+        }
+        System.out.print("Digite um número: ");
+        int n = scanner.nextInt();
+        while (n != media) {
+
+            if (n < media) {
+                System.out.print("Menor\n");
+
+            }else {
+                System.out.print("Maior\n");
+            }
+            System.out.print("Digite um número: ");
+            n = scanner.nextInt();
+        }
+        System.out.print("Acertou!");
+
+    }
 }
