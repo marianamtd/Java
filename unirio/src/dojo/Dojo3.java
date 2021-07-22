a786b716f11d4f45d106bed523a4e02e0d28d28e
marianamtd <mari.duarte2025@gmail.com>
2021-07-22 08:44:20 -0300

    Dojo3

---
+++ unirio/src/dojo/Dojo3.java
@@ -0,0 +1,37 @@
+package dojo;
+import java.util.Scanner;
+
+
+//sem utilizar numeros aleatórios
+/*public class Dojo3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int media = 0, soma = 0;
+
+        System.out.print("Digite os 5 números iniciais: ");
+        for(int i = 0; i < 5; i++) {
+            int num = scanner.nextInt();
+            soma += num;
+            media = soma / 5;
+        }
+        System.out.print("Digite um número: ");
+        int n = scanner.nextInt();
+        while (n != media) {
+
+            if (n < media) {
+                System.out.print("Menor\n");
+
+            }else {
+                System.out.print("Maior\n");
+            }
+            System.out.print("Digite um número: ");
+            n = scanner.nextInt();
+        }
+        System.out.print("Acertou!");
+    }
+} */
+
+//utilizando numeros aleatórios
+public class Dojo3 {
+    public static void main (String[])
+}
