9f980808c6f96af93d942dc8406278a00810ea33
marianamtd <mari.duarte2025@gmail.com>
2021-07-26 08:31:44 -0300

    Update Sequence.java

--- unirio/src/Sequence.java
+++ unirio/src/Sequence.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class Sequence {
     public static void main (String[] args) {   //Método
         Scanner scanner = new Scanner(System.in);
-        int qtd = 0;
+        int qtd = 0, soma = 0;
 
         int num;
         while (true) {
             num = scanner.nextInt();
-            num = num + num;
-            System.out.println(num);
+            soma += num;
             if (num == 0) {
                 break;
             }
             qtd ++;
         }
-
-
+        System.out.println(soma);
     }
 }
