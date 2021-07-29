05e0165089c9b328663d477f4b39b7368016a543
marianamtd <mari.duarte2025@gmail.com>
2021-07-29 09:39:17 -0300

    Create ChattyBot.java

---
+++ unirio/src/hyperskill/ChattyBot.java
@@ -0,0 +1,13 @@
+
+package hyperskill;
+import java.util.Scanner;
+
+public class ChattyBot {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Hello, My name is Flora.\nI was created in 2021");;
+
+    }
+}
