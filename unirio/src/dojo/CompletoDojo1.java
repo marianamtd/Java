6cfbe6973294150d5ae6e0ec935915fd94126900
marianamtd <mari.duarte2025@gmail.com>
2021-07-30 21:04:17 -0300

    Update CompletoDojo1.java

--- unirio/src/dojo/CompletoDojo1.java
+++ unirio/src/dojo/CompletoDojo1.java
@@ -3,42 +3,41 @@ import java.util.Scanner;
 
 public class CompletoDojo1 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        //input do ano e qtd de dias
-        System.out.println("Insira o ano: ");
+        System.out.println("Insira o ano");
         int ano = scanner.nextInt();
-        System.out.println("Insira o dia: ");
-        int quant_dias = scanner.nextInt();
+        System.out.println("Insira dia: ");
+        int qtd_dia = scanner.nextInt();
 
-        //verificar se ele é impar
-        //Acessando a pasta System
-        System.out.println("Paridade: " + (ano % 2 == 0 ? "Par" : "Ímpar"));
-        //verificar se é bissexto
-        System.out.println("Bissexto: " + (((ano % 4 == 0) && (ano % 100 != 0) || (ano % 400 == 0)) ? "true" : "false"));
+        //verificar a qtd de dias
+        while (qtd_dia > 360 || qtd_dia <= 0) {
+            System.out.println("Dia inválido, insira novamente: ");
+            qtd_dia = scanner.nextInt();
+        }
 
-        //verificar distancia
+
+        //verificar a paridade
+        System.out.println("Paridade: " + ((ano % 2 == 0)? "Par" : "Ímpar"));
+
+        //verificar bissexto
+        System.out.println("Bissexto: " + ((ano % 4 == 0) && (ano % 100 != 0) || (ano % 400 == 0)? "True" : "False"));
+
+        //distancia
         int distancia = ano - 2021;
 
         if (distancia < 0)
             distancia *= -1;
-
-        System.out.println("Distância = " + distancia);
-
-        while(quant_dias > 360 || quant_dias <= 0){
-            System.out.println("Dia inválido, insira-o novamente: ");
-            quant_dias = scanner.nextInt();
-        }
+        System.out.println("Distancia: " + distancia);
 
         int dia, mes;
 
-        dia = quant_dias % 30;
-        mes = quant_dias / 30 + 1;
+        dia = qtd_dia % 30;
+        mes = qtd_dia / 30 + 1;
 
-        System.out.printf("%d / %d / %d\n", dia, mes, ano);
-
-        System.out.println("Dias restantes: " + (360 - quant_dias));
+        System.out.printf("%d/%d/%d\n", dia, mes, ano);
+        System.out.println("Dias restante: " + (360 - qtd_dia));
 
     }
 }
+
