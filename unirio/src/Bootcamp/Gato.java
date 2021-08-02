05e1e34daa90170eb8c47660c5120a883676d1d3
marianamtd <mari.duarte2025@gmail.com>
2021-08-02 16:25:01 -0300

    .

--- unirio/src/Bootcamp/Gato.java
+++ unirio/src/Bootcamp/Gato.java
@@ -5,4 +5,50 @@ public class Gato {
     private String cor;
     private Integer idade;
 
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor, Integer idade) {    //aperta alt + insert
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    //para criar um arquivo, so apertar o pacote ou o scr e apertar as teclas alt + insert
+
+
+    public String getNome() {   //aperta alt + insert
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    
 }
