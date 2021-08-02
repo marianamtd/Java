e4c45dec3d7ddca0b8d2011816ccb67880e72a5c
marianamtd <mari.duarte2025@gmail.com>
2021-08-02 16:35:44 -0300

    .

--- cursoemvideo/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module CursoEmVideo {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens sample;
-}
--- unirio/src/Bootcamp/Teste.java
+++ unirio/src/Bootcamp/Teste.java
@@ -3,7 +3,9 @@ package Bootcamp;
 public class Teste {
     public static void main(String[] args) {  //so digitar main q já vai direto
         System.out.println("Hello"); //sout
-    }
+        // para comentar é so apertar crtl + shift + /
 
+        Gato gato = new Gato();
     }
+    
 }
