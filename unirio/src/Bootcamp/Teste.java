490d0a9d9766f3da06175c0b2ed9d6ee36a9bedc
marianamtd <mari.duarte2025@gmail.com>
2021-08-02 16:15:07 -0300

    .

---
+++ unirio/src/Bootcamp/Teste.java
@@ -0,0 +1,11 @@
+package Bootcamp;
+
+public class Teste {
+    public static void main(String[] args) {  //so digitar main q já vai direto
+        System.out.println("Hello"); //sout
+
+
+    }
+
+    }
+}
