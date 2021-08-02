e4ad290eafc6cca0b9bfdc27fd0b2613097e2c98
marianamtd <mari.duarte2025@gmail.com>
2021-08-02 16:16:39 -0300

    .

---
+++ unirio/src/Bootcamp/Gato.java
@@ -0,0 +1,8 @@
+package Bootcamp;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+}
--- unirio/src/Bootcamp/Teste.java
+++ unirio/src/Bootcamp/Teste.java
@@ -3,8 +3,6 @@ package Bootcamp;
 public class Teste {
     public static void main(String[] args) {  //so digitar main q já vai direto
         System.out.println("Hello"); //sout
-
-
     }
 
     }
