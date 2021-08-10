fc8e4ac72e37949be4c7a5e459bb19d88a0009fa
marianamtd <mari.duarte2025@gmail.com>
2021-08-10 20:02:58 -0300

    .

---
+++ unirio/src/SumofIntergersFOR.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumofIntergersFOR {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int sum = 0;
+        for(int i = a; i <= b; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
--- unirio/src/hyperskill/ChattyBot.java
+++ unirio/src/hyperskill/ChattyBot.java
@@ -1,5 +1,4 @@
 
-package hyperskill;
 import java.util.Scanner;
 
 public class ChattyBot {
@@ -7,7 +6,13 @@ public class ChattyBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Hello, My name is Flora.\nI was created in 2021");;
+        System.out.println("Hello, My name is Flora.\n" +
+        "I was created in 2021");
+        System.out.println("Please, remind me your name.");
+        System.out.println("What a great name you have, "+ scanner.nextLine());
+        /*String name = scanner.nextLine();
+        System.out.printf("What a great name you have, %s", name);*/
+
 
     }
 }
