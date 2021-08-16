c2a4689904cbd4ef91ab34be82a5080b204822d5
marianamtd <mari.duarte2025@gmail.com>
2021-08-16 16:13:37 -0300

    Create aula3.java

---
+++ unirio/src/cursoemvideo/aula3/aula3.java
@@ -0,0 +1,14 @@
+package cursoemvideo.aula3;
+
+public class aula3 {
+    public static void main(String[] args) {
+        Esmalte esmalte = new Esmalte(); //instanciar
+        esmalte.marca = "Haskell";
+        esmalte.cor ="Beijo de menta";
+        esmalte.textura = "Cintilante";
+        esmalte.status();
+        esmalte.pintar();
+
+    }
+
+}
