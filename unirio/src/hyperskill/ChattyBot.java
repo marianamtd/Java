c8718efd2c19f777baa1dcb4e01c7259bdfca256
marianamtd <mari.duarte2025@gmail.com>
2021-08-21 12:29:54 -0300

    Update ChattyBot.java

--- unirio/src/hyperskill/ChattyBot.java
+++ unirio/src/hyperskill/ChattyBot.java
@@ -10,8 +10,21 @@ public class ChattyBot {
         "I was created in 2021");
         System.out.println("Please, remind me your name.");
         System.out.println("What a great name you have, "+ scanner.nextLine());
-        String name = scanner.nextLine();
-        System.out.printf("What a great name you have, %s", name);
+
+        //fazer a declaração da var e só depois imprimir o nome
+        /*String name = scanner.nextLine();
+        System.out.printf("What a great name you have, %s", name);*/
+        System.out.println("Let me guess your age.");
+
+        //descobrir a idade do user
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
+
 
 
     }
