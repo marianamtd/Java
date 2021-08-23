cf4b182c41e585c34fc39a1b65410c6b0dd69e12
marianamtd <mari.duarte2025@gmail.com>
2021-08-23 16:50:30 -0300

    Update Dojo3.java

--- unirio/src/dojo/Dojo2/Dojo3.java
+++ unirio/src/dojo/Dojo2/Dojo3.java
@@ -5,5 +5,14 @@ public class Dojo3 {
         //instanciamento
         Pokemon pokemon = new Pokemon();
 
+        pokemon.setNome("Charmander");
+        pokemon.setTipo("Normal");
+        pokemon.setPeso(6.5f);
+        pokemon.setAltura(0.3f);
+        pokemon.setFelicidade(85);
+        pokemon.setChanceDeCaptura(99);
+        pokemon.statusAtual();
+        pokemon.falar();
+
     }
 }
