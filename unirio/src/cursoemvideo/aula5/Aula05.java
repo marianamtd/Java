78b3d58cf60fa7a49bcf7532b29f409d5506e5f0
marianamtd <mari.duarte2025@gmail.com>
2021-08-23 15:08:00 -0300

    Update Aula05.java

--- unirio/src/cursoemvideo/aula5/Aula05.java
+++ unirio/src/cursoemvideo/aula5/Aula05.java
@@ -1,4 +1,29 @@
 package cursoemvideo.aula5;
 
 public class Aula05 {
+    public static void main(String[] args) {
+        ContaCorrente p1 = new ContaCorrente();
+        p1.setNumConta(1642);
+        p1.setNomeDono("Mariana");
+        p1.statusAtual();
+        p1.abrirConta("CP");
+        p1.depositar(2000);
+        p1.getValorSaldo();
+        p1.statusAtual();
+
+        ContaCorrente p2 = new ContaCorrente();
+        p2.setNumConta(2655);
+        p2.setNomeDono("Glaucia");
+        p2.abrirConta("CC");
+        p2.statusAtual();
+        p2.depositar(120500);
+        p2.pagarMensal();
+        p2.statusAtual();
+        p2.sacar(120500);
+        p2.getValorSaldo();
+        p2.statusAtual();
+        p2.sacar(38);
+        p2.fecharConta();
+        p2.statusAtual();
+    }
 }
