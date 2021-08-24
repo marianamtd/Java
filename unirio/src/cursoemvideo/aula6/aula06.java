2de00388f406da95d130ed03d34b41feeee75c27
marianamtd <mari.duarte2025@gmail.com>
2021-08-24 12:06:14 -0300

    Create aula06.java

---
+++ unirio/src/cursoemvideo/aula6/aula06.java
@@ -0,0 +1,9 @@
+package cursoemvideo.aula6;
+
+public class aula06 {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+        controle.setVolume(80);
+        controle.abrirMenu();
+    }
+}
