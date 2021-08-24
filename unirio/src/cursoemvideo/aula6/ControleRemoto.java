bb9e270d6e1129d5c32a30f76425e30354bda2c7
marianamtd <mari.duarte2025@gmail.com>
2021-08-24 12:06:20 -0300

    Create ControleRemoto.java

---
+++ unirio/src/cursoemvideo/aula6/ControleRemoto.java
@@ -0,0 +1,100 @@
+package cursoemvideo.aula6;
+
+public class ControleRemoto implements Controlador{
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+
+    //metodos especiais
+    public ControleRemoto() {
+        this.setLigado(false);
+        this.setTocando(false);
+        this.setVolume(50);
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    //métodos abstratos
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.isLigado());
+        System.out.println("Está tocando? " + this.isTocando());
+        System.out.print("Volume: ");
+        for (int i = 0; i < this.getVolume(); i += 10) {
+            System.out.print("|");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu...");
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void desligarMudo() {
+
+    }
+
+    @Override
+    public void plat() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    //implementar os métodos abstratos
+
+
+}
