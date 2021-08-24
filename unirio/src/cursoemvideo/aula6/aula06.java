4e68896319f46c362ddf17e0b597dce9ea5dd035
marianamtd <mari.duarte2025@gmail.com>
2021-08-24 14:15:12 -0300

    Update aula06.java

--- unirio/src/cursoemvideo/aula6/aula06.java
+++ unirio/src/cursoemvideo/aula6/aula06.java
@@ -3,13 +3,8 @@ package cursoemvideo.aula6;
 public class aula06 {
     public static void main(String[] args) {
         ControleRemoto controle = new ControleRemoto();
-        controle.ligar();
         controle.play();
-        controle.ligarMudo();
-        controle.desligarMudo();
-        controle.pause();
         controle.abrirMenu();
-        controle.fecharMenu();
-        controle.desligar();
+        controle.ligar();
     }
 }
