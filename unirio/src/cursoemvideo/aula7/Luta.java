d7ff97b5c0bac9ed58d279d1b8d898cbbd475f25
marianamtd <mari.duarte2025@gmail.com>
2021-09-01 10:57:33 -0300

    Update Luta.java

--- unirio/src/cursoemvideo/aula7/Luta.java
+++ unirio/src/cursoemvideo/aula7/Luta.java
@@ -1,31 +1,72 @@
 package cursoemvideo.aula7;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Luta {
-    private String desafiado, desafiante;
+    //pode criar um tipo abstrato
+    private Lutador desafiado, desafiante;
     private int rounds;
     private boolean aprovada;
 
-    public void marcarLuta() {
-
+    public void marcarLuta(Lutador lutador1, Lutador lutador2) {
+        if (Objects.equals(lutador1.getCategoria(), lutador2.getCategoria())) {
+            if (lutador1 != lutador2) {
+                this.setAprovada(true);
+                lutador1 = desafiado;
+                lutador2 = desafiante;
+            }
+        }
+        else {
+            desafiado = null;
+            desafiante = null;
+            this.setAprovada(false);
+        }
     }
     public void luta() {
+        Random random = new Random();
 
+        if (this.isAprovada()) {
+            desafiado.apresentar();
+            desafiante.apresentar();
+            int aleatorio = random.nextInt(2);
+            System.out.println(aleatorio);
+
+            if (aleatorio == 0) {
+                System.out.println("Empate!");
+                desafiante.getEmpates();
+                desafiado.getEmpates();
+            }
+            else if (aleatorio == 1) {
+                System.out.println("O" + this.getDesafiado() + "ganhou");
+                desafiado.getVitorias();
+                desafiante.getDerrotas();
+            }
+            else {
+                System.out.println("O" + this.getDesafiante() + "ganhou!!");
+                desafiante.getVitorias();
+                desafiado.getDerrotas();
+            }
+        }
+        else {
+            System.out.println("Luta não pode  acontecer");
+        }
     }
 
     //metodos acessores
-    public String getDesafiado() {
+    public Lutador getDesafiado() {
         return desafiado;
     }
 
-    public void setDesafiado(String desafiado) {
+    public void setDesafiado(Lutador desafiado) {
         this.desafiado = desafiado;
     }
 
-    public String getDesafiante() {
+    public Lutador getDesafiante() {
         return desafiante;
     }
 
-    public void setDesafiante(String desafiante) {
+    public void setDesafiante(Lutador desafiante) {
         this.desafiante = desafiante;
     }
 
