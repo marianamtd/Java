bd3d85670a9af5fcaf498be34374ef0b37adb494
marianamtd <mari.duarte2025@gmail.com>
2021-09-08 14:26:54 -0300

    Create Aula9.java

---
+++ unirio/src/cursoemvideo/aula9/Aula9.java
@@ -0,0 +1,11 @@
+package cursoemvideo.aula9;
+
+import java.util.Scanner;
+
+public class Aula9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
