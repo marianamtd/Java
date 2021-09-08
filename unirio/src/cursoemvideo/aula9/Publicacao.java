f64e8b5f8da1d1af187d8e45bfebb8dd342909b9
marianamtd <mari.duarte2025@gmail.com>
2021-09-08 13:56:13 -0300

    Create Publicacao.java

---
+++ unirio/src/cursoemvideo/aula9/Publicacao.java
@@ -0,0 +1,10 @@
+package cursoemvideo.aula9;
+
+public interface Publicacao {
+
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear();
+    public abstract void avançarPag();
+    public abstract void voltarPag();
+}
