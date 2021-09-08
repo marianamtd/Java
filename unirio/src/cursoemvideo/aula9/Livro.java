312e3f9966d9176b6478303b32a8fc8edc809348
marianamtd <mari.duarte2025@gmail.com>
2021-09-08 13:56:16 -0300

    Create Livro.java

---
+++ unirio/src/cursoemvideo/aula9/Livro.java
@@ -0,0 +1,98 @@
+package cursoemvideo.aula9;
+
+public class Livro implements Publicacao {
+    private String titulo, autor;
+    private int totPaginas, pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    //metodos especiais
+    public void detalhes() {
+
+    }
+
+    //metoodos acessores
+
+    public Livro(String titulo, String autor, int totPaginas, int pagAtual, boolean aberto, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        this.pagAtual = pagAtual;
+        this.aberto = aberto;
+        this.leitor = leitor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    //implementar os metodos abstratos
+    @Override
+    public void abrir() {
+
+    }
+
+    @Override
+    public void fechar() {
+
+    }
+
+    @Override
+    public void folhear() {
+
+    }
+
+    @Override
+    public void avançarPag() {
+
+    }
+
+    @Override
+    public void voltarPag() {
+
+    }
+}
