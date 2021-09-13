52be6faaeeef1f61a3d678bc9fec3b832a0b9a14
marianamtd <mari.duarte2025@gmail.com>
2021-09-13 09:17:45 -0300

    Update Publicacao.java

--- unirio/src/cursoemvideo/aula9/Publicacao.java
+++ unirio/src/cursoemvideo/aula9/Publicacao.java
@@ -4,7 +4,7 @@ public interface Publicacao {
 
     public abstract void abrir();
     public abstract void fechar();
-    public abstract void folhear();
+    public abstract void folhear(int p);
     public abstract void avancarPag();
     public abstract void voltarPag();
 }
