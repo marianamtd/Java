fe509af027bf065b713c3d9dcae7cf7afc4cde57
marianamtd <mari.duarte2025@gmail.com>
2021-09-17 10:16:25 -0300

    Update Pessoa.java

--- unirio/src/cursoemvideo/aula11/Pessoa.java
+++ unirio/src/cursoemvideo/aula11/Pessoa.java
@@ -5,8 +5,42 @@ public abstract class Pessoa {
     private int idade;
 
     //metodo final, não pode ser subscrita
-    
-    public final void fazerAni() {
 
+    public final void fazerAni() {
+        setIdade(getIdade() + 1);
+
+    }
+
+    //metodo acessores
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String detalhes() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ",\n sexo='" + sexo + '\'' +
+                ",\n idade=" + idade +
+                '}';
     }
 }
