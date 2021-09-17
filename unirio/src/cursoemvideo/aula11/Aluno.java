e001725260e7c47390a2b31523fa1514e170b671
marianamtd <mari.duarte2025@gmail.com>
2021-09-17 09:47:28 -0300

    Create Aluno.java

---
+++ unirio/src/cursoemvideo/aula11/Aluno.java
@@ -0,0 +1,12 @@
+package cursoemvideo.aula11;
+
+public class Aluno extends Pessoa{
+    //herança para diferença
+    private int matricula;
+    private String curso;
+
+    //metodos
+    public void pagarMensalidade() {
+
+    }
+}
