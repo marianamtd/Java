bf5eb9806e526e9f0d36137e82291adfc2a799f2
marianamtd <mari.duarte2025@gmail.com>
2021-09-18 12:39:24 -0300

    Update Bolsista.java

--- unirio/src/cursoemvideo/aula11/Bolsista.java
+++ unirio/src/cursoemvideo/aula11/Bolsista.java
@@ -4,12 +4,12 @@ public class Bolsista extends Aluno{
     private int bolsa;
 
     public void renovarBolsa() {
-
+        System.out.println("Renovando bolsa de " + this.getNome());
     }
 
     @Override //sobrepor o metodo pagar mensalidade
     public void pagarMensalidade (){
-
+        System.out.println("Pagando mensalidade");
     }
 
 }
