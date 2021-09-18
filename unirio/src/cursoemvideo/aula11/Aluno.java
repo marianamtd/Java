190965e9afcf9f771f6e9cafdaf4a01554da8a42
marianamtd <mari.duarte2025@gmail.com>
2021-09-18 12:39:27 -0300

    Update Aluno.java

--- unirio/src/cursoemvideo/aula11/Aluno.java
+++ unirio/src/cursoemvideo/aula11/Aluno.java
@@ -7,6 +7,24 @@ public class Aluno extends Pessoa{
 
     //metodos
     public void pagarMensalidade() {
+        System.out.println("Pagando mensalidade de alune " + this.getNome());
+    }
 
+    //metodos acessores
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
     }
 }
