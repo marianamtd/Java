648f71c9dda559c2d9d5e8280c7eee19bfd7ba35
marianamtd <mari.duarte2025@gmail.com>
2021-09-20 09:06:14 -0300

    Create Tempo.java

---
+++ unirio/src/dojo/dojo3/Tempo.java
@@ -0,0 +1,18 @@
+package dojo.dojo3;
+
+public class Tempo {
+
+    public static int[] avancar(int diaAtual, int mesAtual, int anoAtual){
+        int diaNovo = diaAtual;
+        int mesNovo = mesAtual + 1;
+        int anoNovo = anoAtual;
+
+        if (mesNovo == 13){
+            mesNovo = 1;
+            anoNovo += 1;
+        }
+
+        int[] dataNova = {diaNovo, mesNovo, anoNovo};
+        return dataNova;
+    }
+}
