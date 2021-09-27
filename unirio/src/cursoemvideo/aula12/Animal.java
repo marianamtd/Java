294d8b98bacaf58674d39d93b9da2ccde7171b91
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:09:46 -0300

    Update Animal.java

--- unirio/src/cursoemvideo/aula12/Animal.java
+++ unirio/src/cursoemvideo/aula12/Animal.java
@@ -4,13 +4,7 @@ public abstract class Animal {
     protected float peso;
     protected int idade, membros;
 
-    public void locomover() {
-
-    }
-    public void alimentar() {
-
-    }
-    public void emitirSom() {
-
-    }
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
 }
