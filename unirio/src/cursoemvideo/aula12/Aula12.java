b9a12cb213d08031cd635c99ba33f444feb48dc1
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:18:30 -0300

    Create Aula12.java

---
+++ unirio/src/cursoemvideo/aula12/Aula12.java
@@ -0,0 +1,9 @@
+package cursoemvideo.aula12;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        Mamifero mamifero = new Mamifero();
+        Reptil reptil = new Reptil();
+        
+    }
+}
