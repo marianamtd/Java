726e30706f8113311424d9d8c26a0df9020dd4c2
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:09:40 -0300

    Update Mamifero.java

--- unirio/src/cursoemvideo/aula12/Mamifero.java
+++ unirio/src/cursoemvideo/aula12/Mamifero.java
@@ -3,4 +3,18 @@ package cursoemvideo.aula12;
 public class Mamifero extends Animal{
     private String corPelo;
 
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamífero");
+    }
 }
