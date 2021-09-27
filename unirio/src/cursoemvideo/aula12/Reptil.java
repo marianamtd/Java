b1207457ade453aadacf851cd12648e91304dfea
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:09:50 -0300

    Update Reptil.java

--- unirio/src/cursoemvideo/aula12/Reptil.java
+++ unirio/src/cursoemvideo/aula12/Reptil.java
@@ -2,4 +2,19 @@ package cursoemvideo.aula12;
 
 public class Reptil extends Animal{
     private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println();
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public void emitirSom() {
+
+    }
 }
