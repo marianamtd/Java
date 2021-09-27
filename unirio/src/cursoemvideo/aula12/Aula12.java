74170d5065d148b00f285dcd79fd7ae8aac4e176
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:21:47 -0300

    Update Aula12.java

--- unirio/src/cursoemvideo/aula12/Aula12.java
+++ unirio/src/cursoemvideo/aula12/Aula12.java
@@ -4,6 +4,17 @@ public class Aula12 {
     public static void main(String[] args) {
         Mamifero mamifero = new Mamifero();
         Reptil reptil = new Reptil();
-        
+        Peixe peixe = new Peixe();
+        Ave ave = new Ave();
+
+        mamifero.setCorPelo("Amarelo com pintas marrons");
+        mamifero.setIdade(5);
+        mamifero.setPeso(112.67f);
+        mamifero.setMembros(4);
+        mamifero.locomover();
+        mamifero.emitirSom();
+        mamifero.alimentar();
+
+
     }
 }
