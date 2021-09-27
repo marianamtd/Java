c7107ce299546e47b6f43f3c751f8ee74c9b2d35
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:18:34 -0300

    Update Ave.java

--- unirio/src/cursoemvideo/aula12/Ave.java
+++ unirio/src/cursoemvideo/aula12/Ave.java
@@ -7,4 +7,19 @@ public class Ave extends Animal{
         System.out.println("Fazendo ninho...");
         System.out.println("'\\'_//");
     }
+
+    @Override
+    public void locomover() {
+        System.out.println("Voando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo frutas");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de ave");
+    }
 }
