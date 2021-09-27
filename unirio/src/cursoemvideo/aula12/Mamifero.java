4d069d7701a5db72460c2dc457240f6e4b4bf35a
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:18:37 -0300

    Update Mamifero.java

--- unirio/src/cursoemvideo/aula12/Mamifero.java
+++ unirio/src/cursoemvideo/aula12/Mamifero.java
@@ -17,4 +17,12 @@ public class Mamifero extends Animal{
     public void emitirSom() {
         System.out.println("Som de mamífero");
     }
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
 }
