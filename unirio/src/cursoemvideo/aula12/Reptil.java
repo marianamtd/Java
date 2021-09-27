a98113507bac783768e278e7fe84fedbd01c9de1
marianamtd <mari.duarte2025@gmail.com>
2021-09-27 12:13:55 -0300

    Update Reptil.java

--- unirio/src/cursoemvideo/aula12/Reptil.java
+++ unirio/src/cursoemvideo/aula12/Reptil.java
@@ -5,16 +5,16 @@ public class Reptil extends Animal{
 
     @Override
     public void locomover() {
-        System.out.println();
+        System.out.println("Rastejando");
     }
 
     @Override
     public void alimentar() {
-
+        System.out.println("Comendo Vegetais");
     }
 
     @Override
     public void emitirSom() {
-
+        System.out.println("Som de Réptil");
     }
 }
