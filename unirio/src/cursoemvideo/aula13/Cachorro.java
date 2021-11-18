f7b533bcc47211212235e4d73f91e6b134d91bec
marianamtd <mari.duarte2025@gmail.com>
2021-11-18 14:26:52 -0300

    Update Cachorro.java

--- unirio/src/cursoemvideo/aula13/Cachorro.java
+++ unirio/src/cursoemvideo/aula13/Cachorro.java
@@ -10,6 +10,7 @@ public class Cachorro extends Lobo{
     public void reagir (String frase) {
         if(frase.equals("toma comida") || frase.equals("ola")){
             System.out.println("Abanar e latir");
+            this.emitirSom();
         }
         else {
             System.out.println("Rosnar");
@@ -20,8 +21,9 @@ public class Cachorro extends Lobo{
         if (hora < 12){
             System.out.println("Abanar");
         }
-        else if (hora < 18 ){
+        else if (hora <= 18 ){
             System.out.println("Abanar e latir");
+            this.emitirSom();
         }
         else{
             System.out.println("Ignorar");
@@ -30,7 +32,29 @@ public class Cachorro extends Lobo{
 
     public void reagir (Boolean dono){
         if (dono) {
-            
+            System.out.println("Abanar");
         }
+        else {
+            System.out.println("Rosnar e latir");
+            this.emitirSom();
+        }
+    }
+
+    public void reagir (int idade, float peso){
+        if ( idade <= 5) {
+            if (peso <= 10)
+                System.out.println("Abanar");
+            else {
+                System.out.println("Latir");
+                this.emitirSom();
+            }
+        }
+        else {
+            if (peso < 10)
+                System.out.println("Rosnar");
+            else
+                System.out.println("Ignorar");
+        }
+
     }
 }
