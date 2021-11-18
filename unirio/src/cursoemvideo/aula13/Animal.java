2f548c46cf4de1b95b1d40db846b61831d166020
marianamtd <mari.duarte2025@gmail.com>
2021-11-18 14:26:57 -0300

    Update Animal.java

--- unirio/src/cursoemvideo/aula13/Animal.java
+++ unirio/src/cursoemvideo/aula13/Animal.java
@@ -5,4 +5,28 @@ public abstract class Animal {
     protected int idade, membros;
 
     public abstract void emitirSom();
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
 }
