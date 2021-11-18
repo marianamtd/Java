b20b47314962596f2ead4d15bd10d01784ce9691
marianamtd <mari.duarte2025@gmail.com>
2021-11-18 14:26:55 -0300

    Update Aula13.java

--- unirio/src/cursoemvideo/aula13/Aula13.java
+++ unirio/src/cursoemvideo/aula13/Aula13.java
@@ -8,5 +8,11 @@ public class Aula13 {
         l.emitirSom();
         Cachorro c = new Cachorro();
         c.emitirSom();
+        c.reagir(false);
+        c.reagir(5,20);
+        c.reagir("ola");
+        c.reagir(12,19);
+        //c.reagir(11,50);
+        c.reagir(12,19.5f);
     }
 }
