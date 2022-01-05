632466870003e104413f2c62a04a71f0c1d90e8e
marianamtd <mari.duarte2025@gmail.com>
2022-01-05 09:37:19 -0300

    Update Exer1Aula19.java

--- unirio/src/modulo1/arrays/Exer1Aula19.java
+++ unirio/src/modulo1/arrays/Exer1Aula19.java
@@ -6,9 +6,8 @@ public class Exer1Aula19 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] B = new int[5];
-
         int[] A = new int[5];
+        int[] B = new int[A.length];
         for (int i = 0; i < A.length; i++){
             A[i] = scanner.nextInt();
 
